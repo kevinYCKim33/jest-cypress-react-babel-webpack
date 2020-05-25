3f914cab7cf3fd25ba70c145a683fa79a3f9e63b
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-25 19:19:32 -0400

    superheros snapshot testing done

--- src/other/__tests__/super-heros.js
+++ src/other/__tests__/super-heros.js
@@ -2,10 +2,41 @@ import getFlyingSuperHeros from '../super-heros'
 
 test('returns returns super heros that can fly', () => {
   const flyingHeros = getFlyingSuperHeros()
-  //   console.log(flyingHeros)
-  expect(flyingHeros).toEqual([
-    {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
-    {name: 'Apogee', powers: ['gravity control', 'fly']},
-    // {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
-  ])
+  //   console.log(flyingHeros) // instead of doing this arduously...
+  //   expect(flyingHeros).toEqual([
+  //     {name: 'Dynaguy', powers: ['disintegration ray', 'fly']},
+  //     {name: 'Apogee', powers: ['gravity control', 'fly']},
+  //     {name: 'Jack-Jack', powers: ['shapeshifting', 'fly']},
+  //   ])
+
+  // instead of doing the above arduously...
+  //   npm tests -- -u // command to update all of our snapshots
+  //   expect(flyingHeros).toMatchSnapshot() // serializes  result to string
+
+  // inline snapshot is sooo much better!!
+  expect(flyingHeros).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "name": "Dynaguy",
+        "powers": Array [
+          "disintegration ray",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Apogee",
+        "powers": Array [
+          "gravity control",
+          "fly",
+        ],
+      },
+      Object {
+        "name": "Jack-Jack",
+        "powers": Array [
+          "shapeshifting",
+          "fly",
+        ],
+      },
+    ]
+  `)
 })
