c9676212795c90b6bc77c7b1a2fb7ba5560d7b09
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-25 19:35:01 -0400

    finish 8 of 24 emotion jest serializer

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,5 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  snapshotSerializers: ['jest-emotion'],
 }
--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -6,9 +6,20 @@ test('renders', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
   //   console.log(container.innerHTML) // container is a dom node
   expect(container).toMatchInlineSnapshot(`
+    .emotion-0 {
+      position: relative;
+      color: white;
+      background: #1c191c;
+      line-height: 130px;
+      font-size: 6em;
+      -webkit-flex: 1;
+      -ms-flex: 1;
+      flex: 1;
+    }
+
     <div>
       <div
-        class="css-lq9ahq-calculator-display--CalculatorDisplay"
+        class="emotion-0"
         id="calculator-display"
       >
         <div
@@ -24,3 +35,7 @@ test('renders', () => {
 })
 // oohhh gets nicely formatted by jest
 // this works because I have prettier installed
+// class="css-lq9ahq-calculator-display--CalculatorDisplay"
+// ^ ^ completely useless emotion generated css val...
+// need npm install --save-dev jest-emotion
+// see snapshotSerializers in jest.config.js
