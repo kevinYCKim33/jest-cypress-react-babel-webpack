8aed761de1550bebe4a08eb1919fead67ecd3d10
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-25 17:47:47 -0400

    finish 6 of 24 support webpack css modules w. jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
-// any modules that end in css, have it resolve with style-mock.js
+// priority! any module that starts with module.css use idenity-obj-proxy
+
+// else if! any modules that end in css, have it resolve with style-mock.js
+
+// order matters on these moduleNameMapper!
 module.exports = {
   testEnvironment: 'jest-environment-jsdom', // 'jest-environment-node' will break console.log(window)
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -3,5 +3,36 @@ import {render} from '@testing-library/react' // some magic RTL function
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-  render(<AutoScalingText />)
+  const {debug} = render(<AutoScalingText />)
+  debug()
 })
+
+/*
+// there should be a className but there isn't!
+// why? because it's getting turn't to that style.mock.js file just returning {}
+<body>
+  <div>
+    <div
+      data-testid="total"
+      style="transform: scale(1,1);"
+    />
+  </div>
+</body> 
+*/
+
+// npm install --save dev identity-obj-proxy
+
+// now we get autoScalingText along with jest.config.js
+//   moduleNameMapper: {
+// '\\.module\\.css$': 'identity-obj-proxy',
+/*
+<body>
+  <div>
+    <div
+      class="autoScalingText"
+      data-testid="total"
+      style="transform: scale(1,1);"
+    />
+  </div>
+</body>
+*/
